4e3cacaf54f67ac58246b47f8b3bf04c57bb8532
josemorales-g360 <Jose_Morales@cw.groups360.com>
2024-06-27 17:51:33 -0600

    using config

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const { SuccessResponseObject } = require('../common/http');
 const demo = require('./demo.route');
+const propella = require('./propella.route');
 
 const r = Router();
 
 r.use('/demo', demo);
+r.use('/propella', propella);
 
 r.get('/', (req, res) => res.json(new SuccessResponseObject('express vercel boiler plate')));
 
